d6e6906bf3c8c2cda7ca3932c9864f1d901b0a24
Phil Dier <phil@dier.us>
2014-09-15 21:24:06 -0500

    mount volume on client

--- cookbooks/glusterfs/providers/mount.rb
+++ cookbooks/glusterfs/providers/mount.rb
@@ -8,12 +8,23 @@ action :mount do
 	directory new_resource.mount_point
 	directory gluster_dir
 
-	template "#{gluster_dir}/#{volume_name}.vol" do
+	device = new_resource.device || new_resource.mount_point
+
+	volume_file = "#{gluster_dir}/#{volume_name}.vol"
+
+	template volume_file do
 		source "volume.vol.erb"
 		variables ({
 				:servers => new_resource.servers,
 				:device => device
 				})
+	end
+
+	mount new_resource.mount_point do
+		device volume_file
+		fstype "glusterfs"
+		pass 0
+		action [:mount,:enable]
 	end
 
 end
