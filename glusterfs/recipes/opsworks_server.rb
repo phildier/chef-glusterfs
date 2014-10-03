1d0605ba978282cc38d85bb78785509a756b32a3
Phil Dier <phil@dier.us>
2014-10-02 22:42:39 -0500

    add retries

--- glusterfs/recipes/opsworks_server.rb
+++ glusterfs/recipes/opsworks_server.rb
@@ -20,4 +20,6 @@ glusterfs_volume node[:glusterfs][:volume_name] do
 	peers gluster_peers
 	device node[:glusterfs][:device]
 	mount_point node[:glusterfs][:mount_point]
+	retries 5
+	retry_delay 60
 end
